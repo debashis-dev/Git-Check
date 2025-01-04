f6a7511f42486c18dc50cc193a07dbb4c675f648
Mdeba1998 <mdebashis627@gmail.com>
2025-01-04 18:25:43 +0530

    Finalll...

--- src/main/java/com/git/controller/MyController.java
+++ src/main/java/com/git/controller/MyController.java
@@ -1,10 +1,11 @@
 package com.git.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
-	
+	public void myCheck() {
+		
+	}
 }
