8af839ba4ea221bae650bbdde43b0552c6811cfa
Mdeba1998 <mdebashis627@gmail.com>
2025-01-05 20:32:42 +0530

    Checking...

--- src/main/java/com/git/controller/MathController.java
+++ src/main/java/com/git/controller/MathController.java
@@ -17,12 +17,8 @@ public class MathController {
         System.out.println("Div");
     }
 	
-	public void hello() {
-		System.out.println("Hello");
-	}
-	
-	public void hi() {
-		System.out.println("Hi...");
+	public boolean isOk() {
+		return true;
 	}
 }
 
